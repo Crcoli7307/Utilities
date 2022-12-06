602e2dda471d9857163db33752bad48ab4b10391
XBlueSkyCC <xbluesky.cc@gmail.com>
2022-12-06 20:54:10 +0700

    feat(nichhider): custom name/id for diff game

--- NickHider.lua
+++ NickHider.lua
@@ -2,23 +2,34 @@ repeat
     task.wait()
 until game:IsLoaded()
 
+--// Modules
+local Utilities = loadstring(game:HttpGet("https://raw.githubusercontent.com/XBlueSkyCC/Utilities/main/Utilities.lua"))()
+
 --// Services
 local Players = game:GetService("Players")
 
+--// Variables
 local LocalPlayer = Players.LocalPlayer
 
---// Variables
 local Settings = {
-    ["Name"] = "XBlueSkyCC",
-    ["UserId"] = "1"
+    ["All"] = {
+        ["Name"] = "XBlueSkyCC",
+        ["UserId"] = "1"
+    },
+    ["4520749081"] = {
+        ["Name"] = Utilities.RandomString(math.random(10, 15)),
+        ["UserId"] = "991117111" -- Who the fuck is this??
+    }
 }
 
 --// Core
 function HideNick(Object)
+    local _Settings = Settings[game.GameId] or Settings["All"]
+
     local function _(Object, Property)
-        Object[Property] = Object[Property]:gsub(LocalPlayer.Name, Settings["Name"])
-        Object[Property] = Object[Property]:gsub(LocalPlayer.DisplayName, Settings["Name"])
-        Object[Property] = Object[Property]:gsub(LocalPlayer.UserId, Settings["UserId"])
+        Object[Property] = Object[Property]:gsub(LocalPlayer.Name, _Settings["Name"])
+        Object[Property] = Object[Property]:gsub(LocalPlayer.DisplayName, _Settings["Name"])
+        Object[Property] = Object[Property]:gsub(LocalPlayer.UserId, _Settings["UserId"])
     end
 
     if Object:IsA("TextLabel") or Object:IsA("TextButton") then
